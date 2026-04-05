e9813f6e104879dcca99fddcc3c4b7aaca4f6468
Atay Moya <112656287+ataymo1@users.noreply.github.com>
2026-04-04 19:47:47 -0700

    Add: minimum-cost-for-tickets - submission-1

---
+++ Data Structures & Algorithms/minimum-cost-for-tickets/submission-1.py
@@ -0,0 +1,27 @@
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+
+        dp = {}
+        
+        def dfs(i):
+            if i == len(days):
+                return 0
+            if i in dp:
+                return dp[i]
+            
+            dp[i] = float('inf')
+            j = i
+            
+            for added, cost in zip([1, 7, 30], costs):
+                while j < len(days) and days[j] < days[i] + added:
+                    j += 1
+                dp[i] = min(dp[i], cost + dfs(j))
+            
+            return dp[i]
+        return dfs(0)
+
+
+            
+
+        
