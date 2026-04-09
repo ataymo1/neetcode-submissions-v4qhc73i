3c87ac7c35e7f174793e3d139a3f3eda4de8cfbd
Atay Moya <112656287+ataymo1@users.noreply.github.com>
2026-04-08 18:39:59 -0700

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,50 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        treasure_chests = []
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0:
+                    treasure_chests.append(tuple((i, j)))
+        if not treasure_chests:
+            return
+        queue = deque()
+        queue.append(treasure_chests)
+        self.bfs(queue, grid)
+
+    def bfs(self, queue, grid):
+        distance = 0
+        visited = set()
+
+        while queue:
+            level = queue.popleft()
+            next_level = []
+            
+            for pos in level:
+                row, col = pos
+                if distance > grid[row][col] or pos in visited:
+                    continue
+
+                visited.add(pos)
+                grid[row][col] = distance
+
+                if row + 1 < len(grid) and grid[row + 1][col] != -1 and grid[row + 1][col] > distance + 1:
+                    next_level.append(tuple((row + 1, col)))
+                if row - 1 >= 0 and grid[row - 1][col] != -1 and grid[row - 1][col] > distance + 1:
+                    next_level.append(tuple((row - 1, col)))
+                if col + 1 < len(grid[0]) and grid[row][col + 1] != -1 and grid[row][col + 1] > distance + 1:
+                    next_level.append(tuple((row, col + 1)))
+                if col - 1 >= 0 and grid[row][col - 1] != -1 and grid[row][col - 1] > distance + 1:
+                    next_level.append(tuple((row, col - 1)))
+            if next_level:
+                queue.append(next_level)
+            distance += 1
+
+
+                
+
+
+
+
+
+        
+        
